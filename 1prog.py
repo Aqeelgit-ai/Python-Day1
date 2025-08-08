0c0ced48628c462724097a44226fc131b79b9738
AQEEL AHMED KHAN <ak6752279@gmail.com>
2025-08-09 01:49:02 +0530

    Add files via upload

---
+++ 1prog.py
@@ -0,0 +1,46 @@
+print("hellow word ,my name is aaqeel ") # dekho difference in first and second line
+print("hellow word my name is aqeel")
+# age=24 # age variable hai yaha and number likhe tho no need of ""(double codes )
+print("aqeel is hero",19)
+name="aqeel"# aapan double code and single dono bhi use kar sak the but loga doube code use kar the and name is variabe here
+age = 25
+print(age , name)#agar mai print bolke double code mai likhe tho vo jo likha vo  print hoja tha but aapan ko aqeel and  age print hona hai na tho dont use ""
+print ("my name is :",name)
+print("my age is :",age)
+# aab variabke ko variable mai assign kai sa karna dekho <right hand side se left hand side value assign ho tha like age2=age age mai jo bhi hai vo age2 mai jaaatha >
+age2=age
+print(age2)# string can be word or sentence or paragraph kuch bhi reh sak tha basically jo  bhi english ma likh the aapn so
+#type kai sa find karna dekre aapna aab ye type bhi ek fuction
+print(type(age))
+print(type(name))#str matlab string
+#use of boolen and none type
+age=2
+old=False# YE NONE  and false true mai first letter shoud be always capital
+a=None
+print(type(age))
+print(type(a))
+# sum of two nummbers
+a=30
+b=44
+sum=a+b
+print(sum)#yaha per aapna sum ke place mai a+b bhi liksak the
+#assignment operator
+num=20
+num=num+40#aaapan is ke jagah per aapan {num+=40} bhi liksak the
+print(num)
+# logical operator
+print(not False )
+print(not True)
+# and operator
+var1=True
+var2=True
+print("and operator:",var1 and var2 )# agar mai ek var1 ya var2 ko false likha tho answwer false aatha if both are true then true 
+#or operator
+var1=True
+var2=False
+print("or operator kaai sa work  ho tha ",var1 or var2) #agar aapan : ye nai dale tho bhi kuch nai hotha it is only ofr saparetion of answer and question
+# mini task  by gpt
+a="my name is  aqeel,"
+b= "iam 19 years old,"
+c="ai is my favorite"
+print(a,b,c)
